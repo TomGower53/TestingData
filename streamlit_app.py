20caa25004bce494a0caddf2f952aef2fa44166c
TomGower53 <t.gower1@nhs.net>
2025-12-12 17:08:46 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,15 @@ customer_name = st.text_input('Purchaser Name:')
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+
 sales_member = session.table("TEST_DATABASE.PUBLIC.SALES_TEAM").select(col('Sales Team Name')).filter(col('Current_Employee')==1)
 #st.dataframe(data=sales_member, use_container_width=True)
-st.selectbox('Sales Representative:', sales_member, index=None)
+sales_rep_string = st.selectbox('Sales Representative:', sales_member, index=None)
+
+vehicle_list = session.table("TEST_DATABASE.PUBLIC.VEHICLE_OPTIONS").select(col('"Vehicle"'),col('"Lift Capacity (KG)"'),col('"Engine Power (hp)"'),col('"Lift Height (m)"'),col('"Maximum Speed (km/h)"'),col('"Cost"'))
+
+
+if sales_rep_string:
+
+    st.subheader('Vehicles')
+    st.dataframe(data=vehicle_list, use_container_width=True)
