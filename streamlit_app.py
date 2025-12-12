7a0d17ae9446098f9615641171f0f74d4e5b9b4d
TomGower53 <t.gower1@nhs.net>
2025-12-12 16:27:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from snowflake.snowpark.functions import col
 
 
 # Write directly to the app
+st.image('https://hydnum.com/wp-content/uploads/2020/11/avant-logo-new-png-1.png', width="content")
 st.title(f":tractor: Machine Order")
 
 customer_name = st.text_input('Purchaser Name:')
@@ -13,4 +14,5 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 sales_member = session.table("TEST_DATABASE.PUBLIC.SALES_TEAM").select(col('Sales Team Name')).filter(col('Current_Employee')==1)
-st.dataframe(data=sales_member, use_container_width=True)
+#st.dataframe(data=sales_member, use_container_width=True)
+st.selectbox('Sales Representative:', sales_member, index=None)
