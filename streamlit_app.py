f5f5226841528c7f3bbc59f47d79cd06a9b6a3f0
TomGower53 <t.gower1@nhs.net>
2025-12-15 08:43:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,27 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-    
+
 # Write directly to the app
 st.image('https://hydnum.com/wp-content/uploads/2020/11/avant-logo-new-png-1.png', width="content")
 st.title(f":tractor: Machine Order")
 
-customer_name = st.text_input('Purchaser Name:')
-
 # Get the current credentials
 cnx = st.connection("snowflake")
 session = cnx.session()
+
+def clear_text():
+    st.session_state["input"] = ""
+    
+customer_name = st.text_input('Purchaser Name:')
+name_placeholder = st.empty()
+    
+if st.button('Submit order', on_click=clear_text):
+    customer_name.empty()
+
+st.stop()
+
+customer_name = st.text_input('Purchaser Name:')
 
 if customer_name:
 
